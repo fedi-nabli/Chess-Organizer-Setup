0bb2a7a21946ba404947a654ac11a0cd674cfc44
fedi-nabli <fedinabli@gmail.com>
2023-10-09 23:16:48 +0100

    Removed unused imports

--- src/auth.py
+++ src/auth.py
@@ -5,8 +5,6 @@ import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 class AuthModule():
   def __init__(self) -> None:
