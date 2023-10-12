b269a91789c09837c9398c22b5ec6962a455b242
fedi-nabli <fedinabli@gmail.com>
2023-10-12 15:53:21 +0100

    Changed sheets function to a module

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from src.auth import AuthModule
 from src.utils.get_files import DriveModule
-from src.utils.get_values import get_values
+from src.utils.get_values import SpreadsheetModule
 
 def main():
   auth_session = AuthModule()
   drive  = DriveModule(auth_session.creds)
-  get_values(drive.list_files()[2].get('id'), 'A1:B50', auth_session.creds)
+  spreadsheets = drive.list_files()
+  spreadsheet_instance = SpreadsheetModule(auth_session.creds)
+  spreadsheet_instance.get_values(spreadsheets[2].get('id'), 'A1:B50')
 
 if __name__ == '__main__':
   main()
--- src/utils/get_values.py
+++ src/utils/get_values.py
@@ -5,18 +5,26 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-def get_values(spreadsheet_id, range_name, creds: Credentials):
-  try:
-    services = build('sheets', 'v4', credentials=creds)
-    result = services.spreadsheets().values().get(
-      spreadsheetId=spreadsheet_id, range=range_name
-    ).execute()
+class SpreadsheetModule():
+  def __init__(self, creds: Credentials) -> None:
+    try:
+      self.services = build('sheets', 'v4', credentials=creds)
+      print('Initiated sheets module successfully')
 
-    rows = result.get('values', [])
-    print(f'{len(rows)} retrieved')
-    print(rows)
-    return rows
+    except HttpError as error:
+      print(f'An error occured: {error}')
 
-  except HttpError as error:
-    print(f'An error eccured: {error}')
-    return error
+  def get_values(self, spreadsheet_id, range_name):
+    try:
+      result = self.services.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id, range=range_name
+      ).execute()
+
+      rows = result.get('values', [])
+      print(f'{len(rows)} retrieved')
+      print(rows)
+      return rows
+
+    except HttpError as error:
+      print(f'An error occured: {error}')
+      return error
